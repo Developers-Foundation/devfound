79275551283e06aa35886c46e761e30387d0bdb9
fadyazmy <fadel@uwo.ca>
2017-02-12 03:41:11 +0800

    more mods

---
+++ organizations2.php
@@ -0,0 +1,201 @@
+<!-- COVER -->
+<div class="main-container">
+    <section class="text-center imagebg space--lg" data-overlay="3">
+        <div class="background-image-holder" style="background: url(&quot;img/landing-21.jpg&quot;); opacity: 1;">
+            <img alt="background" src="img/landing-21.jpg">
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-md-6">
+                    <h1>Organizations</h1>
+
+                </div>
+            </div>
+            <!--end of row-->
+        </div>
+        <!--end of container-->
+    </section>
+
+</div>
+
+<!-- PROCESS -->
+
+<ul class="tabs-content">
+
+
+    <li class="active"><div class="tab__content">
+
+            <section class=" bg--secondary">
+
+                <!-- PROCESS TITLE-->
+                <section class="text-center height-20">
+                    <div class="container pos-vertical-center">
+                        <div class="row">
+                            <div class="col-sm-8">
+                                <h1>Product Timeline</h1>
+                            </div>
+                        </div>
+                        <!--end of row-->
+                    </div>
+                    <!--end of container-->
+                </section>
+
+
+
+                <div class="container">
+
+
+                    <div class="row">
+                        <div class="process-2">
+                            <div class="col-sm-3">
+                                <div class="process__item">
+                                    <h5>Company established 2012
+                                        <br> 4 founding members</h5>
+                                    <p>
+                                        Stack is built with customization and ease-of-use at its core — whether you're a seasoned developer or just starting out, you'll be making attractive sites faster than any traditional HTML template.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="process__item">
+                                    <h5>Succsessfully funded through
+                                        <br> Bray Investments</h5>
+                                    <p>
+                                        Stack is built with customization and ease-of-use at its core — whether you're a seasoned developer or just starting out, you'll be making attractive sites faster than any traditional HTML template.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="process__item">
+                                    <h5>Posted profit
+                                        <br> second quarter 2015</h5>
+                                    <p>
+                                        Stack is built with customization and ease-of-use at its core — whether you're a seasoned developer or just starting out, you'll be making attractive sites faster than any traditional HTML template.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="process__item">
+                                    <h5>Posted profit
+                                        <br> second quarter 2015</h5>
+                                    <p>
+                                        Stack is built with customization and ease-of-use at its core — whether you're a seasoned developer or just starting out, you'll be making attractive sites faster than any traditional HTML template.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!--end process-->
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+        </div></li>
+</ul>
+
+<!-- MAIN -->
+<div class="main-container">
+    <section class="text-center height-50">
+        <div class="container pos-vertical-center">
+            <div class="row">
+                <div class="col-sm-8">
+                    <h1>Full-service Design &amp; Development</h1>
+                    <p class="lead">
+                        From a team of passionate creators working side-by-side with our partners to deliver engaging digital and physical campaigns.
+                    </p>
+                </div>
+            </div>
+            <!--end of row-->
+        </div>
+        <!--end of container-->
+    </section>
+    <section class="switchable switchable--switch">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="height-50 imagebg border--round box-shadow-wide" data-overlay="4">
+                        <div class="background-image-holder" style="background: url(&quot;img/education-1.jpg&quot;); opacity: 1;">
+                            <img alt="background" src="img/education-1.jpg">
+                        </div>
+                        <div class="pos-vertical-center col-sm-6 boxed boxed--lg bg--none">
+                            <h2>Design</h2>
+                            <p class="lead">
+                                Stack offers a clean and contemporary to suit a range of purposes from corporate, tech startup, marketing site to digital storefront.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--end of row-->
+        </div>
+        <!--end of container-->
+    </section>
+    <section class="switchable switchable--switch">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="height-50 imagebg border--round box-shadow-wide" data-overlay="4">
+                        <div class="background-image-holder" style="background: url(&quot;img/inner-5.jpg&quot;); opacity: 1;">
+                            <img alt="background" src="img/inner-5.jpg">
+                        </div>
+                        <div class="pos-vertical-center col-sm-6 boxed boxed--lg bg--none">
+                            <h2>Build</h2>
+                            <p class="lead">
+                                Stack offers a clean and contemporary to suit a range of purposes from corporate, tech startup, marketing site to digital storefront.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--end of row-->
+        </div>
+        <!--end of container-->
+    </section>
+    <section class="switchable switchable--switch">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="height-50 imagebg border--round box-shadow-wide" data-overlay="3">
+                        <div class="background-image-holder" style="background: url(&quot;img/inner-6.jpg&quot;); opacity: 1;">
+                            <img alt="background" src="img/inner-6.jpg">
+                        </div>
+                        <div class="pos-vertical-center col-sm-6 boxed boxed--lg bg--none">
+                            <h2>Support</h2>
+                            <p class="lead">
+                                Stack offers a clean and contemporary to suit a range of purposes from corporate, tech startup, marketing site to digital storefront.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--end of row-->
+        </div>
+        <!--end of container-->
+    </section>
+    <section class="text-center ">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-md-6">
+                    <div class="cta">
+                        <h2>Purchase Stack now and get lifetime free content updates</h2>
+                        <p class="lead">
+                            Each purchase of Stack comes with six months free support — and a lifetime of free content and bug-fix updates.
+                        </p>
+                        <a class="btn btn--primary type--uppercase" href="#">
+                                    <span class="btn__text">
+                                        Apply
+                                    </span>
+
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!--end of row-->
+        </div>
+        <!--end of container-->
+    </section>
+
+
+    <!-- FOOTER -->
+
+</div>
