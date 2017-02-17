6944595d0485efd796692855cbba009df45079c5
fadyazmy <fadel@uwo.ca>
2017-02-17 16:56:15 +0800

    tpm job description

--- web/careers/tpm2.php
+++ web/careers/tpm2.php
@@ -26,12 +26,9 @@
                 <div class="col-sm-8 col-md-7">
                     <h4> </h4>
                     <p>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                    </p>
+                        Developers’ Foundation is looking for a passionate and talented product manager that wants to lead the product team and create amazing things. You enjoy seeing other people grow and know that a team is stronger than every individual. You have a drive to help and inspire those around you.                    </p>
                     <p>
-                        Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-                    </p>
-                </div>
+                        A typical TPM week can include a phone call (some don’t have internet!) with a charity in an inaccessible remote village and working with the product team to define the vision, strategy and design.                </div>
 
                 <div class="col-sm-4 col-md-4">
                     <div class="text-block">
