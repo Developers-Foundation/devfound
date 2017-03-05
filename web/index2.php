c89722fdd6a0409b5e5546e927cca6649d1d6a5e
fadyazmy <fadel@uwo.ca>
2017-03-05 23:16:56 +0800

    home modified, trying to catch lost mosquito in room ..

--- web/index2.php
+++ web/index2.php
@@ -1,24 +1,14 @@
 <!-- COVER -->
 <section class="cover height-80 imagebg text-center" data-overlay="4">
-    <div class="background-image-holder" style="background: url(&quot;img/education-1.jpg&quot;); opacity: 1;">
-        <img alt="background" src="img/education-1.jpg">
+    <div class="background-image-holder" style="background: url(&quot;img/cover1&quot;); opacity: 1;">
+        <img alt="background" src="img/cover1.jpg">
     </div>
     <div class="container pos-vertical-center">
         <div class="row">
             <div class="col-sm-8">
                 <h1>
-                    Build stylish, lean sites with Stack
+                    Design. Build. Empower.
                 </h1>
-                <p class="lead">
-                    Stack offers a clean and contemporary look to suit a range of purposes from corporate, tech startup, marketing site to digital storefront.
-                </p>
-                <div class="modal-instance block">
-                    <div class="video-play-icon video-play-icon--sm modal-trigger" data-modal-index="0"></div>
-                    <span>
-                                        <strong>See us in action</strong>&nbsp;&nbsp;&nbsp;104 Seconds</span>
-
-                    <!--end of modal-container-->
-                </div>
                 <!--end of modal instance-->
             </div>
         </div>
@@ -55,9 +45,6 @@
                                         <p>
                                             Stack comes with integration for Mail Chimp and Campaign Monitor forms - ideal for modern marketing campaigns
                                         </p>
-                                        <a href="#">
-                                            Learn More
-                                        </a>
                                     </div>
                                     <!--end feature-->
                                 </div>
@@ -68,9 +55,6 @@
                                         <p>
                                             Including the premium Icons Mind icon kit, Stack features a highly diverse set of icons suitable for all purposes.
                                         </p>
-                                        <a href="#">
-                                            Learn More
-                                        </a>
                                     </div>
                                     <!--end feature-->
                                 </div>
@@ -80,13 +64,11 @@
                                         <!--
                                         icon-Brush
                                         -->
-                                        <h4>Support</h4>
+                                        <h4>Empower</h4>
                                         <p>
                                             Combine blocks from a range of categories to build pages that are rich in visual style and interactivity
                                         </p>
-                                        <a href="#">
-                                            Learn More
-                                        </a>
+
                                     </div>
                                     <!--end feature-->
                                 </div>
@@ -99,204 +81,75 @@
         </ul>
     </div>
 </section>
-<!-- FEATURE-->
-<div class="tabs-container text-center" data-content-align="left">
-    <ul class="tabs-content">
-        <li class="active"><div class="tab__content">
-                <section class="feature-large switchable bg--secondary">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm-6 col-md-6">
-                                <img alt="Image" class="border--round box-shadow-wide" src="img/inner-5.jpg">
-                            </div>
-                            <div class="col-sm-7 col-md-5 ">
-                                <div class="heading-block">
-                                    <h2>Experience quality design and thoughfully crafted code.</h2>
-                                </div>
-                                <div class="text-block">
-                                    <h5>Premium Design</h5>
-                                    <p>
-                                        Combine blocks from a range of categories to build pages that are rich in visual style and interactivity.
-                                    </p>
-                                </div>
-                                <div class="text-block">
-                                    <h5>Quality Code</h5>
-                                    <p>
-                                        Stack is built with customization and ease-of-use at its core — consistent markup and useful data attribute modifiers make rapid development simple.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <!--end of row-->
-                    </div>
-                    <!--end of container-->
-                </section>
-            </div></li>
-    </ul>
-</div>
-<!-- PORTFOLIO TITLE -->
-<section class="text-center">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
-                <h2>Portfolio</h2>
-                <p class="lead">
-                    Check out some of the incredible organizatons we worked with.
-                </p>
-            </div>
-        </div>
-        <!--end of row-->
-    </div>
-    <!--end of container-->
-</section>
 
-<!-- PORTFOLIO GRID-->
-<section>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="masonry">
 
-                    <div class="row">
-                        <div class="masonry__container masonry--active" style="position: relative; height: 360.954px;">
-                            <div class="masonry__item col-md-4 col-sm-6 col-xs-12 filter-digital" data-masonry-filter="Digital" style="position: absolute; left: 0px; top: 0px;">
-                                <div class="project-thumb hover-element border--round hover--active">
-                                    <a href="#">
-                                        <div class="hover-element__initial">
-                                            <div class="background-image-holder" style="background: url(&quot;img/work-5.jpg&quot;); opacity: 1;">
-                                                <img alt="background" src="img/work-5.jpg">
-                                            </div>
-                                        </div>
-                                        <div class="hover-element__reveal" data-scrim-top="5">
-                                            <div class="project-thumb__title">
-                                                <h4>Nuaca</h4>
-                                                <span>Digital &amp; Interactive Design</span>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                            <!--end item-->
-                            <div class="masonry__item col-md-4 col-sm-6 col-xs-12 filter-print" data-masonry-filter="Print" style="position: absolute; left: 389px; top: 0px;">
-                                <div class="project-thumb hover-element border--round hover--active">
-                                    <a href="#">
-                                        <div class="hover-element__initial">
-                                            <div class="background-image-holder" style="background: url(&quot;img/work-2.jpg&quot;); opacity: 1;">
-                                                <img alt="background" src="img/work-2.jpg">
-                                            </div>
-                                        </div>
-                                        <div class="hover-element__reveal" data-scrim-top="5">
-                                            <div class="project-thumb__title">
-                                                <h4>Get Lost! Thailand</h4>
-                                                <span>Print Marketing</span>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                            <!--end item-->
-                            <div class="masonry__item col-md-4 col-sm-6 col-xs-12 filter-branding" data-masonry-filter="Branding" style="position: absolute; left: 779px; top: 0px;">
-                                <div class="project-thumb hover-element border--round hover--active">
-                                    <a href="#">
-                                        <div class="hover-element__initial">
-                                            <div class="background-image-holder" style="background: url(&quot;img/work-3.jpg&quot;); opacity: 1;">
-                                                <img alt="background" src="img/work-3.jpg">
-                                            </div>
-                                        </div>
-                                        <div class="hover-element__reveal" data-scrim-top="5">
-                                            <div class="project-thumb__title">
-                                                <h4>M&amp;D Stairs Company</h4>
-                                                <span>Branding &amp; Identity</span>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                            <!--end item-->
-                        </div>
-                        <!--end of masonry container-->
-                    </div>
-                    <!--end row-->
+
+
+    <div class="tab__content">
+        <section class="imageblock switchable feature-large bg--secondary">
+            <div class="imageblock__content col-md-6 col-sm-4 pos-right">
+                <div class="background-image-holder" style="background: url(&quot;img/opt_western_fam.jpg&quot;); opacity: 1;">
+                    <img alt="image" src="img/opt_western_fam.jpg">
                 </div>
-                <!--end masonry-->
             </div>
-        </div>
-        <!--end of row-->
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5 col-sm-7">
+                        <h2>Stack is design-driven</h2>
+                        <p class="lead">
+                            Stack offers a clean and contemporary to suit a range of purposes from corporate, tech startup, marketing site to digital storefront. Elements have been designed to showcase content in a diverse yet consistent manner.
+                        </p>
+                        <p class="lead">
+                            Multiple font and colour scheme options mean that dramatically altering the look of your site is just clicks away — Customizing your site in the included Variant Page Builder makes experimenting with styles and content arrangements dead simple.
+                        </p>
+                    </div>
+                </div>
+                <!--end of row-->
+            </div>
+            <!--end of container-->
+        </section>
     </div>
-    <!--end of container-->
-</section>
 
 
 
-
-<!-- TESTIMONIALS-->
-<div class="tabs-container text-center" data-content-align="left">
-
-    <ul class="tabs-content">
-
-        <li class="active"><div class="tab__content">
-                <section class="text-center bg--secondary">
-                    <div class="container">
-
-
-
-                        <!-- TESTIMONIALS TITLE-->
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
-                                    <h2>Testimonials</h2>
-                                    <br>
-                                </div>
-                            </div>
-                            <!--end of row-->
-                        </div>
-
-
-
+<li class="active"><div class="tab__content">
+        <section class="text-center ">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-10 col-md-8">
+                        <h2>Get involved</h2>
+                        <p class="lead">
+                            Stack's visual style is simple yet distinct, making it an ideal starting point for your project whether it be a basic marketing site, or multi-page company presence.
+                        </p>
 
 
 
                         <div class="row">
-                            <div class="col-md-8 col-sm-10">
-                                <div class="slider" data-paging="true" data-children="3">
-                                    <ul class="slides flickity-enabled is-draggable" tabindex="0">
+                            <div class="col-sm-6">
+                                <div class="feature">
+                                    <a class="btn btn--primary" href="#">
+                                        <span class="btn__text">Charities</span>
+                                    </a>
+                            </div>
+                            </div>
 
+                            <div class="col-sm-6">
+                                <div class="feature">
 
-                                        <div class="flickity-viewport" style="height: 0.52px;"><div class="flickity-slider" style="left: 0px; transform: translateX(-100%);"><li class="slide" style="position: absolute; left: 0%;">
-                                                    <div class="testimonial">
-                                                        <blockquote>
-                                                            “There's a feeling of structure that you can't find in other templates — Stack has become my default template.”
-                                                        </blockquote>
-                                                        <h5>Jason Briggs</h5>
-                                                        <span>Envato Customer</span>
-                                                    </div>
-                                                </li><li class="slide is-selected" style="position: absolute; left: 100%;">
-                                                    <div class="testimonial">
-                                                        <blockquote>
-                                                            “Variant has been a massive plus for my workflow — I can now get live mockups out in a matter of hours, my clients really love it.”
-                                                        </blockquote>
-                                                        <h5>Rob Vasquez</h5>
-                                                        <span>Interface Designer — Yoke</span>
-                                                    </div>
-                                                </li><li class="slide" style="position: absolute; left: 200%;">
-                                                    <div class="testimonial">
-                                                        <blockquote>
-                                                            “We’ve been using Stack to prototype designs quickly and efficiently. Needless to say we’re hugely impressed by the style and value.”
-                                                        </blockquote>
-                                                        <h5>Maguerite Holland</h5>
-                                                        <span>Interface Designer — Yoke</span>
-                                                    </div>
-                                                </li></div></div><ol class="flickity-page-dots"><li class="dot"></li><li class="dot is-selected"></li><li class="dot"></li></ol></ul>
+                                    <a class="btn btn--primary" href="#">
+                                        <span class="btn__text">Students</span>
+                                    </a>
                                 </div>
                             </div>
-                        </div>
-                        <!--end of row-->
+
+
+
+
                     </div>
-                    <!--end of container-->
-                </section>
-            </div></li>
-
-
-    </ul>
-</div>
+                </div>
+                <!--end of row-->
+            </div>
+            <!--end of container-->
+        </section>
+    </div></li>
