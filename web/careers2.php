b993e205cefdbab35933948f373381a9eade0245
fadyazmy <fadel@uwo.ca>
2017-03-07 15:14:24 +0800

    careers

--- web/careers2.php
+++ web/careers2.php
@@ -15,65 +15,8 @@
     </section>
 
 
-    <section class="switchable bg--secondary">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 col-md-5">
-                    <ul class="accordion accordion-2 accordion--oneopen">
-                        <li class="active">
-                            <div class="accordion__title">
-                                <span class="h5">Culture</span>
-                            </div>
-                            <div class="accordion__content">
-                                <p class="lead">
-                                    Stack follows the BEM naming convention that focusses on logical code readability that is reflected in both the HTML and CSS. Interactive elements such as accordions and tabs follow the same markup patterns making rapid development easier for developers and beginners alike.
-                                </p>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="accordion__title">
-                                <span class="h5">Learn</span>
-                            </div>
-                            <div class="accordion__content">
-                                <p class="lead">
-                                    Stack offers a clean and contemporary to suit a range of purposes from corporate, tech startup, marketing site to digital storefront. Elements have been designed to showcase content in a diverse yet consistent manner.
-                                </p>
-                                <p class="lead">
-                                    Multiple font and colour scheme options mean that dramatically altering the look of your site is just clicks away &mdash; Customizing your site in the included Variant Page Builder makes experimenting with styles and content arrangements dead simple.
-                                </p>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="accordion__title">
-                                <span class="h5">Experience</span>
-                            </div>
-                            <div class="accordion__content">
-                                <p class="lead">
-                                    Medium Rare is an elite author known for offering high-quality, high-value products backed by timely and personable support. Recognised and awarded by Envato on multiple occasions for producing consistently outstanding products, it's no wonder over 20,000 customers enjoy using Medium Rare templates.
-                                </p>
-                            </div>
-                        </li>
-                    </ul>
-                    <!--end accordion-->
-                </div>
-                <div class="col-md-6 col-sm-6 switchable__text text-center">
-                    <div class="video-cover border--round">
-                        <div class="background-image-holder">
-                            <img alt="image" src="img/education-5.jpg" />
-                        </div>
-                        <div class="video-play-icon"></div>
-                        <iframe src="https://www.youtube.com/embed/6p45ooZOOPo?autoplay=1" allowfullscreen="allowfullscreen"></iframe>
-                    </div>
-                    <!--end video cover-->
-                    <span>Check out the working culture at Stack</span>
-                </div>
-            </div>
-            <!--end of row-->
-        </div>
-        <!--end of container-->
-    </section>
-    <section>
-        <div class="container">
+    <section class=" bg--secondary">
+        <div class="container ">
             <div class="row">
                 <div class="col-sm-12">
                     <div class="heading-block">
@@ -177,7 +120,7 @@
     </section>
 
 
-    <section class="text-center">
+    <section class="text-center ">
         <div class="container">
             <div class="row">
                 <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
