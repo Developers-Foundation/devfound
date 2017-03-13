8766a6840a26b02219c3502533a4f86991058bc6
Nobody Random <iamnobodyrandom@yahoo.com>
2017-03-13 11:35:17 -0400

    add

--- web/careers/marketing.php
+++ web/careers/marketing.php
@@ -1,4 +1,137 @@
 <?php
 include_once ("header.php");
-include_once ("marketing2.php");
+?>
+<div class="main-container">
+    <section class="space--sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1>Marketing</h1>
+                    <ol class="breadcrumbs">
+                        <li>
+                            <a href="../index.php">Home</a>
+                        </li>
+                        <li>
+                            <a href="../career.php">Careers</a>
+                        </li>
+                        <li>Marketing</li>
+                    </ol>
+                    <hr>
+                </div>
+            </div>
+            <!--end of row-->
+        </div>
+        <!--end of container-->
+    </section>
+    <section class="switchable">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-md-7">
+
+                    <h4>
+                        Are you passionate about humanitarian outreach and empowering charities?
+                        Are you a creative mind with a knack for analytics and leadership skills?
+                        We're looking for enthusiastic, driven candidates to join our marketing team
+                        and help us tell our story, the students' and the charities'.
+                    </h4>
+                    <h4>
+                        As part of Developer's Foundation marketing squad,
+                        you will get to be part of shaping our brand,
+                        creating meaningful connections with appropriate audiences,
+                        as well as planning and developing marketing strategies for an everchanging enviromnet.
+                    </h4>
+                </div>
+
+                <div class="col-sm-4 col-md-4">
+                    <div class="text-block">
+                        <h5>Responsibilities</h5>
+                        <ul class="bullets">
+                            <li>Public relations activities with media outlets</li>
+                            <li>Design and produce creative visuals through film, images and graphics</li>
+                            <li>Development and implementation of marketing strategies</li>
+                            <li>Assure the consistent communication of brand image</li>
+                            <li>Brainstorm and plan new, meaningful ways to use communication outlets</li>
+                            <li>Monitor and analyze short-term and long-term progress and success of marketing activities</li>
+                        </ul>
+                    </div>
+                    <div class="text-block">
+                        <h5>Experience</h5>
+                        <li>
+                            Currently enrolled in a marketing/communications program(or similar).
+                        </li>
+                        <li>
+                            Previous marketing experience is a bonus.
+                        </li>
+                        <li>
+                            Photography, videography and editing skills is a bonus.
+                        </li>
+                    </div>
+                </div>
+            </div>
+            <!--end of row-->
+        </div>
+        <!--end of container-->
+    </section>
+    <section class="text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="cta">
+                        <h2>Sound like the job for you?</h2>
+                        <p class="lead">
+                            Got what it takes to work with us? Great! Apply to join our crazy team of innovators.
+                        </p>
+
+                        <a class="btn btn--primary type--uppercase" href="../apply">
+                                    <span class="btn__text">
+                                        Drop your resumé here
+                                    </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!--end of row-->
+        </div>
+        <!--end of container-->
+    </section>
+    <section class="bg--secondary">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h4>Similar Openings</h4>
+                </div>
+                <div class="col-sm-4">
+                    <div class="feature feature-1 boxed boxed--border">
+
+                        <h5>Finance</h5>
+                        <p>
+                            Western, Queen's & Toronto
+
+                        </p>
+                        <h6>
+                            Coming Soon
+                        </h6>
+                    </div>
+                    <!--end feature-->
+                </div>
+                <div class="col-sm-4">
+                    <div class="feature feature-1 boxed boxed--border">
+                        <h5>Product Designer</h5>
+                        <p>
+                            Western, Queen's & Toronto
+                        </p>
+                        <h6 >
+                            Coming Soon
+                        </h6>
+                    </div>
+                    <!--end feature-->
+                </div>
+            </div>
+            <!--end of row-->
+        </div>
+        <!--end of container-->
+    </section>
+</div>
+<?php
 include_once ("footer.php");
+?>
