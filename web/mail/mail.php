c298cbac5f026cc814a16dc63f5d40a8e890a499
fadyazmy <fadel@uwo.ca>
2017-03-14 01:29:32 +0800

    mailer

--- web/mail/mail.php
+++ web/mail/mail.php
@@ -16,7 +16,7 @@ $outgoingServerSecurity     = 'tls';                 // Options: 'ssl' , 'tls' ,
 
 // Sending Account Settings - replace these details with an email account held on the SMTP server entered above.
 // These 2 settings are only required if you choose 'smtp' for emailMethod above.
-$sendingAccountUsername     = 'fadifessa@gmail.com';
+$sendingAccountUsername     = 'fadifessa';
 $sendingAccountPassword     = 'hkuhkuer1';
 
 // Recipient (To:) Details  - Change this to the email details of who will receive all the emails from the website.
