174e96f109726297d8920b53981088d23ece9ac9
Fady Azmy <Fadyazmy@users.noreply.github.com>
2017-04-11 04:20:29 +0800

    designer open

--- public_html/careers2.php
+++ public_html/careers2.php
@@ -71,9 +71,9 @@
                         <p>
                             Western, Queen's & Toronto
                         </p>
-                        <h6>
-                            Coming Soon
-                        </h6>
+                        <a href="careers/designer">
+                            View &amp; Apply
+                        </a>
                     </div>
                     <!--end feature-->
                 </div>
